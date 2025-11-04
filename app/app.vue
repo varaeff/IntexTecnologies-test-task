f5ff025d918858f47e6ca3a1f41fdc4bfd550807
varaeff <varaev.sergey@gmail.com>
2025-11-04 15:28:55 +0400

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <div>
+    <Button class="btn">Click me</Button>
+  </div>
+</template>
+
+<style scoped>
+.btn {
+  cursor: pointer;
+}
+</style>
